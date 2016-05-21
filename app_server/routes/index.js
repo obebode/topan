384bcdbb8c98d91ebd137bb3400bb82edae0f0b8
obebode <obebode@yahoo.com>
2016-05-21 11:20:49 +0100

    Added Procfile and completed heroku setup

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,14 +2,16 @@ var express = require('express');
 var router = express.Router();
 var ctrlMain = require('../controllers/main');
 
+/*var homepageController = function(req, res) {
+	res.render('index', { title: 'Express' });
+};*/
+
 /* GET home page. */
-/*var homepageController = function(req, res, next) {
+/*
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-}; */
+}); */
 
-/* GET home page. */
 router.get('/', ctrlMain.index);
-
-/*router.get('/', homepageController); */
 
 module.exports = router;
